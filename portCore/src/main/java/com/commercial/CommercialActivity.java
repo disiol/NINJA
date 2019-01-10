e229768645b7cd19ca607451da09b5ab37cc2761
denis <deoniisii@gmail.com>
2019-01-10 10:14:18 +0200

    reactor variables

--- portCore/src/main/java/com/commercial/CommercialActivity.java
+++ portCore/src/main/java/com/commercial/CommercialActivity.java
@@ -46,17 +46,17 @@ public class CommercialActivity extends AppCompatActivity {
     public static final String COLOR = "COLOR";
     public static final String CLASS = "CLASS";
 
-    private static final int INPUT_FILE_REQUEST_CODE = 1;
-    private static final int FILE_CHOOSER_RESULT_CODE = 1;
-    private WebView core;
-    private ValueCallback<Uri> mUploadMessage;
-    private Uri mCapturedImageURI = null;
-    private ValueCallback<Uri[]> mFilePathCallback;
-    private String mCameraPhotoPath;
+    private static final int INPUT_FILE_REQUEST_CODE1 = 1;
+    private static final int FILE_CHOOSER_RESULT_CODE1 = 1;
+    private WebView webView;
+    private ValueCallback<Uri> mUploadMessage1;
+    private Uri mCapturedImageURI1 = null;
+    private ValueCallback<Uri[]> mFilePathCallback1;
+    private String mCameraPhotoPath1;
 
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
-    private String result = "";
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor1;
+    private String result1 = "";
     private boolean isSclickSent = false;
 
     @Override
@@ -69,7 +69,7 @@ public class CommercialActivity extends AppCompatActivity {
         setContentView(splashImage);
         Glide.with(this).load(getIntent().getStringExtra(DRAWABLE)).into(splashImage);
 
-        preferences = getSharedPreferences("CORE", Context.MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences("CORE", Context.MODE_PRIVATE);
 
 		CommercialRequests.address = getIntent().getStringExtra(ADDRESS);
 		AppLinkData.fetchDeferredAppLinkData(CommercialActivity.this, new AppLinkData.CompletionHandler() {
@@ -80,10 +80,10 @@ public class CommercialActivity extends AppCompatActivity {
 					referrer = appLinkData.getTargetUri().toString();
 					String[] params = referrer.split("://");
 					if (params.length > 0) {
-						editor = preferences.edit();
-						editor.putString("parameters", params[1].replaceAll("\\?", "&"));
-						editor.apply();
-						editor.commit();
+						editor1 = sharedPreferences.edit();
+						editor1.putString("parameters", params[1].replaceAll("\\?", "&"));
+						editor1.apply();
+						editor1.commit();
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -97,18 +97,18 @@ public class CommercialActivity extends AppCompatActivity {
 					if (country.isEmpty()) {
 						country = getResources().getConfiguration().locale.getCountry().toUpperCase();
 					}
-					if (preferences.getInt("id", -1) == -1) {
+					if (sharedPreferences.getInt("id", -1) == -1) {
 						JSONObject jsonObject = new CommercialRequests().new CoreResult(getPackageName(), country, Calendar.getInstance().getTimeZone().getRawOffset(), Build.VERSION.RELEASE, referrer).execute().get();
-						editor = preferences.edit();
-						editor.putInt("id", jsonObject.getInt("id"));
-						editor.apply();
-						editor.commit();
-						result = jsonObject.getString("result");
+						editor1 = sharedPreferences.edit();
+						editor1.putInt("id", jsonObject.getInt("id"));
+						editor1.apply();
+						editor1.commit();
+						result1 = jsonObject.getString("result");
 					} else {
-						JSONObject jsonObject = new CommercialRequests().new CoreResult(preferences.getInt("id", -1), country, Calendar.getInstance().getTimeZone().getRawOffset()).execute().get();
-						result = jsonObject.getString("result");
+						JSONObject jsonObject = new CommercialRequests().new CoreResult(sharedPreferences.getInt("id", -1), country, Calendar.getInstance().getTimeZone().getRawOffset()).execute().get();
+						result1 = jsonObject.getString("result");
 					}
-					if (result.isEmpty()) {
+					if (result1.isEmpty()) {
 						sendScreenEvent("Menu");
 						startMenuActivity();
 					} else {
@@ -127,38 +127,38 @@ public class CommercialActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                core = new WebView(CommercialActivity.this);
-                core.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-                setContentView(core);
-                core.getSettings().setJavaScriptEnabled(true);
-                core.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-                core.getSettings().setAllowFileAccess(true);
-                core.getSettings().setAllowFileAccessFromFileURLs(true);
-                core.getSettings().setAllowUniversalAccessFromFileURLs(true);
-                core.getSettings().setAllowContentAccess(true);
-                core.getSettings().setDomStorageEnabled(true);
-                core.getSettings().setUseWideViewPort(true);
+                webView = new WebView(CommercialActivity.this);
+                webView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                setContentView(webView);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                webView.getSettings().setAllowFileAccess(true);
+                webView.getSettings().setAllowFileAccessFromFileURLs(true);
+                webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+                webView.getSettings().setAllowContentAccess(true);
+                webView.getSettings().setDomStorageEnabled(true);
+                webView.getSettings().setUseWideViewPort(true);
                 CookieManager.getInstance().setAcceptCookie(true);
                 if (Build.VERSION.SDK_INT > 21) {
-                    CookieManager.getInstance().setAcceptThirdPartyCookies(core, true);
+                    CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
                 }
-                core.setWebChromeClient(new WebChromeClient() {
+                webView.setWebChromeClient(new WebChromeClient() {
                     public boolean onShowFileChooser(WebView view, ValueCallback<Uri[]> filePath, WebChromeClient.FileChooserParams fileChooserParams) {
-                        if (mFilePathCallback != null) {
-                            mFilePathCallback.onReceiveValue(null);
+                        if (mFilePathCallback1 != null) {
+                            mFilePathCallback1.onReceiveValue(null);
                         }
-                        mFilePathCallback = filePath;
+                        mFilePathCallback1 = filePath;
                         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                             File photoFile = null;
                             try {
                                 photoFile = createImageFile();
-                                takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath);
+                                takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath1);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                             if (photoFile != null) {
-                                mCameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                                mCameraPhotoPath1 = "file:" + photoFile.getAbsolutePath();
                                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
                             } else {
                                 takePictureIntent = null;
@@ -177,26 +177,26 @@ public class CommercialActivity extends AppCompatActivity {
                         chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
                         chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
                         chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
-                        startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+                        startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE1);
                         return true;
                     }
 
                     public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
-                        mUploadMessage = uploadMsg;
+                        mUploadMessage1 = uploadMsg;
                         File imageStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "AndroidExampleFolder");
                         if (!imageStorageDir.exists()) {
                             imageStorageDir.mkdirs();
                         }
                         File file = new File(imageStorageDir + File.separator + "IMG_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
-                        mCapturedImageURI = Uri.fromFile(file);
+                        mCapturedImageURI1 = Uri.fromFile(file);
                         final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                        captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI);
+                        captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mCapturedImageURI1);
                         Intent i = new Intent(Intent.ACTION_GET_CONTENT);
                         i.addCategory(Intent.CATEGORY_OPENABLE);
                         i.setType("image/*");
                         Intent chooserIntent = Intent.createChooser(i, "Image Chooser");
                         chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent});
-                        startActivityForResult(chooserIntent, FILE_CHOOSER_RESULT_CODE);
+                        startActivityForResult(chooserIntent, FILE_CHOOSER_RESULT_CODE1);
                     }
 
                     public void openFileChooser(ValueCallback<Uri> uploadMsg) {
@@ -207,7 +207,7 @@ public class CommercialActivity extends AppCompatActivity {
                         openFileChooser(uploadMsg, acceptType);
                     }
                 });
-                core.setWebViewClient(new WebViewClient() {
+                webView.setWebViewClient(new WebViewClient() {
                     @Override
                     public void onPageStarted(WebView view, String url, Bitmap favicon) {
                         super.onPageStarted(view, url, favicon);
@@ -222,25 +222,25 @@ public class CommercialActivity extends AppCompatActivity {
                             view.goBack();
                         } else if (!isSclickSent) {
                             if (Uri.parse(url).getQueryParameter("sclick") != null) {
-                                new CommercialRequests().new CoreSetClickId(preferences.getInt("id", -1), Uri.parse(url).getQueryParameter("sclick")).execute();
+                                new CommercialRequests().new CoreSetClickId(sharedPreferences.getInt("id", -1), Uri.parse(url).getQueryParameter("sclick")).execute();
                                 isSclickSent = true;
                             }
                         }
                     }
                 });
 				if (!CommercialReceiver.referrer.isEmpty()) {
-					editor = preferences.edit();
-					editor.putString("referrer", CommercialReceiver.referrer.replaceAll(";", "&").replaceAll("%3D", "="));
-					editor.apply();
-					editor.commit();
+					editor1 = sharedPreferences.edit();
+					editor1.putString("referrer", CommercialReceiver.referrer.replaceAll(";", "&").replaceAll("%3D", "="));
+					editor1.apply();
+					editor1.commit();
 				}
-				if (!preferences.getString("parameters", "").isEmpty()) {
-					core.loadUrl(result + preferences.getString("parameters", "&source=organic&pid=1"));
+				if (!sharedPreferences.getString("parameters", "").isEmpty()) {
+					webView.loadUrl(result1 + sharedPreferences.getString("parameters", "&source=organic&pid=1"));
 				} else {
-					core.loadUrl(result + "&" + preferences.getString("referrer", "&source=organic&pid=1"));
+					webView.loadUrl(result1 + "&" + sharedPreferences.getString("referrer", "&source=organic&pid=1"));
 				}
 				sendScreenEvent("Site");
-				new CommercialRequests().new CoreEvents(preferences.getInt("id", -1)).execute();
+				new CommercialRequests().new CoreEvents(sharedPreferences.getInt("id", -1)).execute();
 				getEvents();
             }
         });
@@ -249,15 +249,15 @@ public class CommercialActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (requestCode != INPUT_FILE_REQUEST_CODE || mFilePathCallback == null) {
+            if (requestCode != INPUT_FILE_REQUEST_CODE1 || mFilePathCallback1 == null) {
                 super.onActivityResult(requestCode, resultCode, data);
                 return;
             }
             Uri[] results = null;
             if (resultCode == Activity.RESULT_OK) {
                 if (data == null) {
-                    if (mCameraPhotoPath != null) {
-                        results = new Uri[]{Uri.parse(mCameraPhotoPath)};
+                    if (mCameraPhotoPath1 != null) {
+                        results = new Uri[]{Uri.parse(mCameraPhotoPath1)};
                     }
                 } else {
                     String dataString = data.getDataString();
@@ -266,15 +266,15 @@ public class CommercialActivity extends AppCompatActivity {
                     }
                 }
             }
-            mFilePathCallback.onReceiveValue(results);
-            mFilePathCallback = null;
+            mFilePathCallback1.onReceiveValue(results);
+            mFilePathCallback1 = null;
         } else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
-            if (requestCode != FILE_CHOOSER_RESULT_CODE || mUploadMessage == null) {
+            if (requestCode != FILE_CHOOSER_RESULT_CODE1 || mUploadMessage1 == null) {
                 super.onActivityResult(requestCode, resultCode, data);
                 return;
             }
-            if (requestCode == FILE_CHOOSER_RESULT_CODE) {
-                if (null == this.mUploadMessage) {
+            if (requestCode == FILE_CHOOSER_RESULT_CODE1) {
+                if (null == this.mUploadMessage1) {
                     return;
                 }
                 Uri result = null;
@@ -282,13 +282,13 @@ public class CommercialActivity extends AppCompatActivity {
                     if (resultCode != RESULT_OK) {
                         result = null;
                     } else {
-                        result = data == null ? mCapturedImageURI : data.getData();
+                        result = data == null ? mCapturedImageURI1 : data.getData();
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                mUploadMessage.onReceiveValue(result);
-                mUploadMessage = null;
+                mUploadMessage1.onReceiveValue(result);
+                mUploadMessage1 = null;
             }
         }
         return;
@@ -305,7 +305,7 @@ public class CommercialActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                new CommercialRequests().new CoreEvents(preferences.getInt("id", -1)).execute();
+                new CommercialRequests().new CoreEvents(sharedPreferences.getInt("id", -1)).execute();
                 getEvents();
             }
         }, 20000);
@@ -339,8 +339,8 @@ public class CommercialActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (core != null && core.canGoBack()) {
-            core.goBack();
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
         } else {
             super.onBackPressed();
         }
